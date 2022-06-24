2d1801ee2f6f2884b70903df9bc1435ea653f9f1
Adewale Lawal <87212727+wazatarbass@users.noreply.github.com>
2022-06-24 23:42:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+
+
+num1 = parseFloat(parseInt(num1));
+num2 = parseFloat(parseInt(num2));
+
+document.write(num1 + num2);
+document.write(num1 - num2);
+document.write(num1 * num2);
+document.write(num1 / num2);
+
