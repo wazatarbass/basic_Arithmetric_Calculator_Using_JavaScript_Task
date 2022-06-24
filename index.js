abe8d7307f495187097f118f37d7d49aefe22081
Adewale Lawal <87212727+wazatarbass@users.noreply.github.com>
2022-06-24 23:43:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let x,y;
+x =  parseFloat(parseInt(prompt('Enter first number')));
+y =  parseFloat(parseInt(prompt('Enter second number')));
+let z = x + y;
+let a = x - y;
+let b = x * y;
+let c = x / y;
+
+alert("the addition of the given number is:" +z);
+alert("the difference of the given number is:"-a);
+alert("the product of the given number is:"*b);
+alert("the  quotient of the given number is:"/c);
